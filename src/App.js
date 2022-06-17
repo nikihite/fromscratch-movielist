e5aa9b1bb9e09410f69a48eacf273fd7a278aaf2
Niki Hite <nikihite@comcast.net>
2022-06-17 10:33:22 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { useEffect, useState } from 'react';
+//import { useEffect, useState } from 'react';
 import './App.css';
-import MovieForm from './MovieForm';
-import MovieList from './MovieList';
-import MovieItem from './MovieItem';
-import { useMovieForm } from './useMovieForm';
+//import MovieForm from './MovieForm';
+//import MovieList from './MovieList';
+//import MovieItem from './MovieItem';
+//import { useMovieForm } from './useMovieForm';
 
 function App() {
   return (
